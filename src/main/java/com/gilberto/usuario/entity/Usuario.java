62445983fa395499551502fac014082321eefcd7
gilberttobarros <gilbertto.barros03@gmail.com>
2026-06-13 13:35:45 -0300

    Atualizações da repository e entity

--- src/main/java/com/gilberto/usuario/entity/Usuario.java
+++ src/main/java/com/gilberto/usuario/entity/Usuario.java
@@ -1,4 +1,27 @@
 package com.gilberto.usuario.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+@Table(name = "usuario")
+
 public class Usuario {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column(name = "nome", length = 100)
+        private String nome;
+        @Column(name = "email", length = 100)
+        private String email;
+        @Column(name = "senha")
+        private String senha;
+        @OneToMany(cascade = CascadeType.ALL)
+        @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+        private List<Endereco> enderecos;
+        @OneToMany(cascade = CascadeType.ALL)
+        @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+        private List<Telefone> telefones;
+
 }
